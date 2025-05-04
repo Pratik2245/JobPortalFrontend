26d94a07c6df4578de1b3267298dc9ec1976304b
Pratik2245 <paithankarpratik3@gmail.com>
2025-05-04 22:48:23 +0530

    Header added successfully

--- job-portal-app/src/Header/Header.tsx
+++ job-portal-app/src/Header/Header.tsx
@@ -3,31 +3,27 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import { IoSettingsSharp } from "react-icons/io5";
 import { Settings } from "lucide-react";
 import { SiCloudflareworkers } from "react-icons/si";
+import NavLinks from "./NavLinks";
 const Header = () => {
   return (
     <div className="w-full text-white flex justify-between items-center px-6 bg-[#2d2d2d] h-20">
       <div className="flex items-center text-[#ffbd20] gap-2">
-        <span className="font-bold text-3xl">Sparks</span>
         <SiCloudflareworkers size={35} />
+        <span className="font-bold text-3xl">JobSparks</span>
       </div>
-      <div className="flex gap-5 text-l mt-2  items-center">
-        <a href="">Find Job</a>
-        <a href="">Find Jobs</a>
-        <a href="">Upload Jobs</a>
-        <a href="">About Us</a>
-      </div>
+      <NavLinks />
       <div className="flex gap-4 items-center">
         <div className="flex items-center gap-2">
+          <div>Pratik</div>
           <div>
             <Avatar src="avatar1.png" alt="it's me" />
           </div>
-          <div>Pratik</div>
         </div>
         <div className="bg-[#3d3d3d] p-1.5 rounded-full">
           <Settings />
         </div>
         <div className="bg-[#3d3d3d] p-1.5 rounded-full">
-          <Indicator offset={6} size={8} color="green" processing>
+          <Indicator offset={6} size={8} color="bright-sun.4" processing>
             <IoMdNotificationsOutline size={24} />
           </Indicator>
         </div>
---
+++ job-portal-app/src/Header/NavLinks.tsx
@@ -0,0 +1,31 @@
+import { Link, useLocation } from "react-router-dom";
+
+const NavLinks = () => {
+  const links = [
+    { name: "Find Jobs", url: "/find-jobs" },
+    { name: "Find Talent", url: "/find-talent" },
+    { name: "Upload Jobs", url: "/upload-jobs" },
+    { name: "About Us", url: "/about" },
+  ];
+  const location = useLocation();
+  return (
+    <div className="flex gap-5 text-[##d1d1d1] mt-2 h-full  items-center">
+      {links.map((link, index) => (
+        <div
+          key={link.name}
+          className={`${
+            location.pathname === link.url
+              ? "border-t-[#ffbd20] text-[#ffbd20]"
+              : "border-transparent"
+          } border-t-[3px] h-full flex items-center`}
+        >
+          <Link key={index} to={link.url}>
+            {link.name}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NavLinks;
