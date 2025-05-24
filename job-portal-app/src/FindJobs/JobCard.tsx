7aaaf16fac8efdd80763136f32cba98da4e701bb
Pratik2245 <paithankarpratik3@gmail.com>
2025-05-24 23:02:10 +0530

    findjobsPage Done Successfully

--- job-portal-app/src/FindJobs/JobCard.tsx
+++ job-portal-app/src/FindJobs/JobCard.tsx
@@ -1,34 +1,42 @@
-import { Bookmark } from "lucide-react";
+import { Divider, Text } from "@mantine/core";
+import { Bookmark, Clock } from "lucide-react";
 
-const JobCard = () => {
+const JobCard = (props: any) => {
   return (
-    <div className="flex flex-col gap-2 rounded-2xl bg-[#3d3d3d] p-4 w-72 ">
+    <div className="flex flex-col gap-2 rounded-2xl bg-[#3d3d3d] p-4 w-72 hover:shadow-[0_0_5px_1px_yellow] !shadow-[#ffd149">
       <div className="flex justify-between ">
         <div className="flex gap-2 items-center">
           <div className="bg-[#454545]">
-            <img className="h-7" src="/Icons/Microsoft.png" alt="" />
+            <img className="h-7" src={`/Icons/${props.company}.png`} alt="" />
           </div>
           <div className="">
-            <div className="font-semibold">Developer</div>
+            <div className="font-semibold">{props.jobTitle}</div>
             <div className="text-xs text-[#b0b0b0]">
-              Amazon &#x2022; 6 Applicants
+              {props.company} &#x2022; {props.applicants} Applicants
             </div>
           </div>
         </div>
-        <Bookmark />
+        <Bookmark className="cursor-pointer" />
       </div>
-      <div className="flex items-center justify-between gap-2 [&>div]:py-1  [&>div]:bg-[#454545] [&>div]:text-[#ffbd20] [&>div]:rounded-lg text-xs [&>div]:px-2">
-        <div>Intermediate</div>
-        <div>Full Time</div>
-        <div>New York</div>
+      <div className="flex items-center justify-between gap-2 [&>div]:py-1  [&>div]:bg-[#454545] [&>div]:text-[#ffbd20] [&>div]:rounded-lg text-xs [&>div]:px-2 ]">
+        <div>{props.experience}</div>
+        <div>{props.jobType}</div>
+        <div>{props.location}</div>
       </div>
-      <div className="text-sm mb-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam delectus
-        provident eos!
-      </div>
-      <div className="flex justify-between [&>div]:text-[#ffbd20]">
-        <div>33 LPA</div>
-        <div>Posted 1 Month Ago</div>
+      <Text
+        className="!text-xs !text-[#b0b0b0] mb-2 text-justify"
+        lineClamp={3}
+      >
+        {props.description}
+      </Text>
+      <Divider size="xs" color="#4f4f4f" />
+      <div className="flex justify-between">
+        <div className="text-[#d1d1d1] font-semibold">
+          &#8377;{props.package}
+        </div>
+        <div className="flex text-sm gap-1 items-center text-[#888888]">
+          <Clock size={15} /> Posted {props.postedDaysAgo} Month Ago
+        </div>
       </div>
       <div className="border rounded-2xl bg-[#fffbeb] text-[#ffbd20] text-center ">
         View Job
--- job-portal-app/src/FindJobs/Jobs.tsx
+++ job-portal-app/src/FindJobs/Jobs.tsx
@@ -1,16 +1,23 @@
+import { jobList } from "../Data/JobsData";
 import JobCard from "./JobCard";
 import MostRecent from "./MostRecent";
 
 const Jobs = () => {
   return (
     <>
-      <div className="flex justify-between p-3 mt-5">
-        <div className="text-2xl font-semibold">Recommended Jobs</div>
-        <div>
-          <MostRecent />
+      <div className="p-5">
+        <div className="flex justify-between p-3 mt-5">
+          <div className="text-2xl font-semibold">Recommended Jobs</div>
+          <div>
+            <MostRecent />
+          </div>
+        </div>
+        <div className=" mt-10 flex flex-wrap gap-5 justify-around">
+          {jobList.map((job, key) => (
+            <JobCard kwy={key} {...job} />
+          ))}
         </div>
       </div>
-      <JobCard />
     </>
   );
 };
